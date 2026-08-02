e64222fe50fe4820eccaf98d19374873cce0f1fe
Aniket Bera <115163621+beraaniket@users.noreply.github.com>
2026-08-02 07:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (23.58%) - LeetSync

--- leetCodeDaily/486-predict-the-winner/predict-the-winner.java
+++ leetCodeDaily/486-predict-the-winner/predict-the-winner.java
@@ -1,4 +1,5 @@
 class Solution {
+    int[][] t = new int[23][23];
     public int solve(int i, int j, int[] nums){
         if(i > j){
             return 0;
@@ -6,12 +7,18 @@ class Solution {
         if(j == 0){
             return nums[i];
         }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
         int take_i = nums[i] - solve(i + 1, j,nums);
         int take_j = nums[j] - solve(i, j - 1,nums);
-        return Math.max(take_i, take_j);
+        return t[i][j] = Math.max(take_i, take_j);
     }
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
+        for (int[] row : t) {
+            java.util.Arrays.fill(row, -1);
+        }
         return solve(0, n - 1, nums) >= 0;
     }
 }
