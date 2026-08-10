37bcbb704b5b12daecedaaacdc521f69e1351e42
Aniket Bera <115163621+beraaniket@users.noreply.github.com>
2026-08-11 00:21:00 +0530

    Time: 14 ms (91.38%) | Memory: 42.5 MB (68.41%) - LeetSync

---
+++ leetCodeDaily/1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] t = new boolean[n + 1];
+
+        for(int i = 1; i < n + 1; i++){
+            for(int k = 1; k * k <= i; k++){
+                if(t[i - (k * k)] == false){
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return t[n];
+    }
+}
