a41c045fee1d699fb9a80eaed9a9a72eb9decbae
Aniket Bera <115163621+beraaniket@users.noreply.github.com>
2026-08-18 01:28:05 +0530

    Time: 5 ms (83.28%) | Memory: 44 MB (72.95%) - LeetSync

--- Math/1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
+++ Math/1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         int n = arr.length;
         Arrays.sort(arr);
@@ -8,6 +8,19 @@ class Solution {
         }
         for(int i = n - 2; i >= 1; i--){
             if(diff[i] != diff[i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+*/
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        for(int i = 2; i < arr.length; i++){
+            if(diff != (arr[i] - arr[i - 1])){
                 return false;
             }
         }
