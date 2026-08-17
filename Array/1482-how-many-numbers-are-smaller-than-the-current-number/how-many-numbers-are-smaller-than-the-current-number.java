ad8a387e540c14b7d08edc1f230beddfd5088c12
Aniket Bera <115163621+beraaniket@users.noreply.github.com>
2026-08-17 21:27:52 +0530

    Time: 1 ms (99.90%) | Memory: 45.5 MB (51.26%) - LeetSync

--- Array/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ Array/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
         int[] copy = new int[n];
@@ -18,5 +18,27 @@ class Solution {
             nums[i] = mp.get(nums[i]);
         }
         return nums;
+    }
+}*/
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[101];
+        for(int num : nums){
+            freq[num]++;
+        }
+        for(int i = 1; i < 100; i++){
+            freq[i] += freq[i - 1];
+        }
+        int[] result = new int[n];
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                result[i] = 0;
+            }
+            else{
+                result[i] = freq[nums[i] - 1];
+            }
+        }
+        return result;
     }
 }
