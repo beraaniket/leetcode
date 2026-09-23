69b68d9c47446745681ffaae8008ab6305b62f75
Aniket Bera <115163621+beraaniket@users.noreply.github.com>
2026-09-24 00:27:02 +0530

    Time: 4 ms (98.07%) | Memory: 101.7 MB (92.68%) - LeetSync

--- LeetcodeDaily/1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ LeetcodeDaily/1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -5,22 +5,25 @@ class Solution {
         for(int i = 0; i < n; i++){
             k += nums[i];
         }
-        if (k < 0) return -1;
-        Map<Integer, Integer> preSumMap = new HashMap<>();
-        preSumMap.put(0, -1);
-        int sum = 0;
-        int maxLen = (k == 0) ? 0 : -1;
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
+        if(k == 0){
+            return n;
+        }
+        else if(k < 0){
+            return -1;
+        }
+        int left = 0, right = 0;
+        int maxLen = -1;
+        int sum = nums[0];
+        while(right < n){
+            while(left <= right && sum >k){
+                sum -= nums[left];
+                left++;
+            }
             if(sum == k){
-                maxLen = Math.max(maxLen, i + 1);
+                maxLen = Math.max(maxLen, right - left + 1);
             }
-            int rem = sum - k;
-            if(preSumMap.containsKey(rem)){
-                int len = i - preSumMap.get(rem);
-                maxLen = Math.max(maxLen, len);
-            }
-            preSumMap.putIfAbsent(sum, i);
+            right++;
+            if(right < n) sum += nums[right];
         }
         return maxLen == -1 ? -1 : n - maxLen;
     }
